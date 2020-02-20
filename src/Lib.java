0ee40b16c1848e074fa86b36c6df9d70cb3b4ea9
marcelam28 <motiu_m@yahoo.com>
2020-02-20 20:22:24 +0200

    initial commit

---
+++ src/Lib.java
@@ -0,0 +1,21 @@
+
+public class Lib {
+	public int nrCarti;
+	public int nrZileInregistrare;
+	public int nrCartiPerZi;
+	public int[] idCarti;
+	
+	public Lib(int nrC, int nrZile, int nrCp) {
+		nrCarti = nrC;
+		nrZileInregistrare = nrZile;
+		nrCartiPerZi = nrCp;
+	}
+	
+	public void setIdCarti(int[] idCarti) {
+		idCarti = idCarti.clone();
+	}
+	
+	public String toString() {
+		return nrCarti + " ";
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] argv) throws FileNotFoundException, IOException {
+		try (BufferedReader br = new BufferedReader(new FileReader("a_example.txt"))) {
+			String line;
+			line = br.readLine();
+
+			String[] splited = line.split("\\s+");
+			int nrCarti = Integer.parseInt(splited[0]);
+			int nrLib = Integer.parseInt(splited[1]);
+			int nrZile = Integer.parseInt(splited[2]);
+
+			line = br.readLine();
+			splited = line.split("\\s+");
+			int[] scorCarti = Arrays.stream(splited).mapToInt(Integer::parseInt).toArray();
+
+			ArrayList<Lib> biblioteci = new ArrayList<Lib>(nrLib);
+
+			int i;
+			for (i = 0; i < nrLib; i++) {
+				line = br.readLine();
+				splited = line.split("\\s+");
+				Lib lib = new Lib(Integer.parseInt(splited[0]), Integer.parseInt(splited[1]),
+						Integer.parseInt(splited[2]));
+
+				biblioteci.add(lib);
+
+				line = br.readLine();
+				splited = line.split("\\s+");
+				int[] idCarti = Arrays.stream(splited).mapToInt(Integer::parseInt).toArray();
+				lib.setIdCarti(idCarti);
+			}
+
+			System.out.print(biblioteci);
+		}
+	}
+}
